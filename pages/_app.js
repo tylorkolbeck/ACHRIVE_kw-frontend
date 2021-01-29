4b0a5e91023521b780fe18e3af65946a941de27c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-29 18:44:06 -0500

    udpate strapi API env variable

--- pages/_app.js
+++ pages/_app.js
@@ -1,49 +1,50 @@
 import App from 'next/app'
 import Head from 'next/head'
-import Cookie from "js-cookie"
+import Cookie from 'js-cookie'
 import '../assets/css/style.css'
 import { createContext, useState, useEffect } from 'react'
-import AppContext from "../context/AppContext";
+import AppContext from '../context/AppContext'
 import { getStrapiMedia } from '../lib/media'
 import { fetchAPI } from '../lib/api'
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337";
+const API_URL =
+  process.env.NEXT_PUBLIC_STRAPI_API_URL || 'http://localhost:1337'
 
 // Store Strapi Global object in context
 export const GlobalContext = createContext({})
 
 const MyApp = ({ Component, pageProps }) => {
   const { global } = pageProps
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
-      window.addEventListener('beforeunload', Cookie.remove('user'));
+    window.addEventListener('beforeunload', Cookie.remove('user'))
     return () => {
-      window.removeEventListener('beforeunload', Cookie.remove('user'));
-    };
+      window.removeEventListener('beforeunload', Cookie.remove('user'))
+    }
   }, [])
 
   useEffect(() => {
     // grab token value from cookie
-    const token = Cookie.get("token");
+    const token = Cookie.get('token')
 
     if (token) {
       // authenticate the token on the server and place set user object
       fetch(`${API_URL}/users/me`, {
         headers: {
-          Authorization: `Bearer ${token}`,
-        },
+          Authorization: `Bearer ${token}`
+        }
       }).then(async (res) => {
         // if res comes back not valid, token is not valid
         // delete the token and log the user out on client
         if (!res.ok) {
-          Cookie.remove("token");
-          setUser(null);
-          return null;
+          Cookie.remove('token')
+          setUser(null)
+          return null
         }
-        const user = await res.json();
-        setUser({user: user});
-      });
+        const user = await res.json()
+        setUser({ user: user })
+      })
     }
   }, [])
 
@@ -53,26 +54,26 @@ const MyApp = ({ Component, pageProps }) => {
         value={{
           user: user,
           isAuthenticated: !!user,
-          setUser: setUser,
+          setUser: setUser
         }}
       >
-      <Head>
-        <link rel='shortcut icon' href={getStrapiMedia(global.favicon)} />
-        <link
-          rel='stylesheet'
-          href='https://fonts.googleapis.com/css?family=Staatliches'
-        />
-        <link
-          rel='stylesheet'
-          href='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css'
-        />
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js' />
-        <script src='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js' />
-        <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js' />
-      </Head>
-      <GlobalContext.Provider value={global}>
-        <Component {...pageProps} />
-      </GlobalContext.Provider>
+        <Head>
+          <link rel='shortcut icon' href={getStrapiMedia(global.favicon)} />
+          <link
+            rel='stylesheet'
+            href='https://fonts.googleapis.com/css?family=Staatliches'
+          />
+          <link
+            rel='stylesheet'
+            href='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/css/uikit.min.css'
+          />
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.min.js' />
+          <script src='https://cdn.jsdelivr.net/npm/uikit@3.2.3/dist/js/uikit-icons.min.js' />
+          <script src='https://cdnjs.cloudflare.com/ajax/libs/uikit/3.2.0/js/uikit.js' />
+        </Head>
+        <GlobalContext.Provider value={global}>
+          <Component {...pageProps} />
+        </GlobalContext.Provider>
       </AppContext.Provider>
     </>
   )
