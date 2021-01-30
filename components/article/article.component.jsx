a67843499c42dc0078a3b70c8b9bd058a03f593c
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-01-30 13:20:13 -0500

    fix getStrapiMedia so that no global images dont crash the frontend

--- components/article/article.component.jsx
+++ components/article/article.component.jsx
@@ -2,21 +2,26 @@ import React from 'react'
 import Card from '../card/card.component'
 
 const Articles = ({ articles }) => {
-  const leftArticlesCount = Math.ceil(articles.length / 5)
-  const leftArticles = articles.slice(0, leftArticlesCount)
-  const rightArticles = articles.slice(leftArticlesCount, articles.length)
+  // const leftArticlesCount = Math.ceil(articles.length / 5)
+  // const leftArticles = articles.slice(0, leftArticlesCount)
+  // const rightArticles = articles.slice(leftArticlesCount, articles.length)
 
   return (
     <div>
       <div className='uk-child-width-1-2@s' data-uk-grid='true'>
         <div>
-          {leftArticles.map((article, i) => {
+          {articles.map((article, i) => {
             return (
               <Card article={article} key={`article__left__${article.slug}`} />
             )
           })}
+          {/* {leftArticles.map((article, i) => {
+            return (
+              <Card article={article} key={`article__left__${article.slug}`} />
+            )
+          })} */}
         </div>
-        <div>
+        {/* <div>
           <div className='uk-child-width-1-2@m uk-grid-match' data-uk-grid>
             {rightArticles.map((article, i) => {
               return (
@@ -27,7 +32,7 @@ const Articles = ({ articles }) => {
               )
             })}
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Articles from '../components/article/article.component'
+import Article from '../components/article/article.component'
 import Layout from '../components/layout/layout.component'
 import Seo from '../components/seo/seo.component'
 import { fetchAPI } from '../lib/api'
@@ -10,8 +10,8 @@ const Home = ({ articles, categories, homepage }) => {
       <Seo seo={homepage.seo} />
       <div className='uk-section'>
         <div className='uk-container uk-container-large'>
-          <h1>{homepage.hero.title}</h1>
-          <Articles articles={articles} />
+          <h1>{homepage?.hero?.title}</h1>
+          <Article articles={articles} />
         </div>
       </div>
     </Layout>
@@ -20,8 +20,9 @@ const Home = ({ articles, categories, homepage }) => {
 
 export async function getStaticProps() {
   // Run API calls in parallel
+
   const [articles, categories, homepage] = await Promise.all([
-    fetchAPI('/articles?status=published'),
+    fetchAPI('/articles'),
     fetchAPI('/categories'),
     fetchAPI('/homepage')
   ])
