dfb6451c90348d5befcca83ff16bf37c7b6bea11
tylor.kolbeck@gmail.com <tylor.kolbeck@gmail.com>
2021-02-26 13:24:13 -0500

    add privacy policy and risk disclosure

--- components/Footer/Footer.component.jsx
+++ components/Footer/Footer.component.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
 import Link from 'next/link'
-import { Grid, Typography } from '@material-ui/core'
+import { Grid } from '@material-ui/core'
 import { appLinks } from '../../lib/app.links'
+import BodyText from '../Typography/BodyText/BodyText.component'
 
 const useStyles = makeStyles((theme) => {
   return {
@@ -15,9 +16,6 @@ const useStyles = makeStyles((theme) => {
     link: {
       marginRight: theme.spacing(2),
       color: 'white'
-    },
-    caption: {
-      marginTop: theme.spacing(10)
     }
   }
 })
@@ -26,34 +24,60 @@ export default function Footer() {
   const classes = useStyles()
   return (
     <div className={classes.root}>
-      <Grid container justify="space-between">
-        <Grid item>Copyright Killer Whale 2021</Grid>
-        <Grid item>
-          <Grid container>
-            {appLinks.map((link) => (
-              <Link href={link.url} key={link.label}>
-                <a className={classes.link} key={link.label}>
-                  {link.label}
-                </a>
-              </Link>
-            ))}
+      <Grid container alignItems="space-between">
+        <Grid item sm={6}>
+          <Grid container style={{ marginBottom: '20px' }}>
+            <Grid item xs={12}>
+              <BodyText
+                variant="caption"
+                color="white"
+                className={classes.caption}
+              >
+                View our{' '}
+                <a href="/privacypolicy" style={{ color: 'rgb(82, 196, 237)' }}>
+                  Privacy Policy
+                </a>{' '}
+              </BodyText>
+            </Grid>
+            <Grid item xs={12}>
+              <BodyText
+                variant="caption"
+                color="white"
+                className={classes.caption}
+              >
+                View our{' '}
+                <a
+                  href="/risk-disclosure"
+                  style={{ color: 'rgb(82, 196, 237)' }}
+                >
+                  Risk Disclosure
+                </a>{' '}
+              </BodyText>
+            </Grid>
           </Grid>
         </Grid>
+
+        <Grid
+          item
+          sm={6}
+          style={{
+            display: 'flex',
+            justifyContent: 'flex-end'
+          }}
+        >
+          {appLinks.map((link) => (
+            <Link href={link.url} key={link.label}>
+              <a className={classes.link} key={link.label}>
+                {link?.label.toUpperCase()}
+              </a>
+            </Link>
+          ))}
+        </Grid>
       </Grid>
-      <Grid container>
-        <Typography variant="caption" className={classes.caption}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam qui
-          a possimus fuga nostrum cum quod modi, suscipit, est numquam assumenda
-          saepe quidem officia consequatur aperiam dolore similique. Facilis
-          veritatis alias, distinctio eum fugit laborum adipisci in delectus
-          reprehenderit sit deserunt, fuga, illum nemo aliquid qui maxime
-          nostrum fugiat. Suscipit tempora ab voluptatum dolorum? Quibusdam
-          necessitatibus natus dolorem praesentium quasi sint, voluptas quaerat
-          similique omnis deleniti quisquam saepe eius sequi mollitia cumque
-          minus cum iusto harum maxime? Natus fuga, iusto labore excepturi ea
-          mollitia alias! Veniam omnis facere odit asperiores aliquam veritatis,
-          consequuntur enim qui aliquid ab ratione vel pariatur?
-        </Typography>
+
+      <Grid container justify="space-between">
+        <Grid item>Copyright Killer Whale 2021</Grid>
+        <Grid item></Grid>
       </Grid>
     </div>
   )
