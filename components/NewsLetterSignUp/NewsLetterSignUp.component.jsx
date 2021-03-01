50607d8ed6488c475082a10ae2af20131adad24e
jkaplanis <johntkaplanis@gmail.com>
2021-02-28 18:13:20 -0800

    resolve merge conflicts

--- components/NewsLetterSignUp/NewsLetterSignUp.component.jsx
+++ components/NewsLetterSignUp/NewsLetterSignUp.component.jsx
@@ -28,8 +28,6 @@ const useStyles = makeStyles((theme) => ({
   },
   emailInput: {
     width: '100%',
-
-    marginBottom: '20px',
     '& input': {
       background: 'white',
       borderRadius: '4px',
@@ -116,8 +114,20 @@ export default function NewsLetterSignUp() {
                     onChange={(event) => handleChange(event)}
                     helperText={errorMsg && errorMsg}
                   />
+                  <CaptionText>
+          {successMsg ? (
+            'Thanks for signing up!'
+          ) : (
+            <span style={{ fontWeight: 'normal', fontSize: '12px' }}>
+              * We do not spam or sell your info.{' '}
+              <span className={classes.privacyPolicyLink}>
+                <a href="/privacypolicy">Privacy Policy</a>
+              </span>
+            </span>
+          )}
+        </CaptionText>
                 </Grid>
-                <Grid item xs={12} sm={12} md={2}>
+                <Grid item xs={12} >
                   <Button
                     type="button"
                     variant="contained"
@@ -132,18 +142,7 @@ export default function NewsLetterSignUp() {
             </Grid>
           </form>
         )}
-        <CaptionText>
-          {successMsg ? (
-            'Thanks for signing up!'
-          ) : (
-            <span style={{ fontWeight: 'normal', fontSize: '12px' }}>
-              * We do not spam or sell your info.{' '}
-              <span className={classes.privacyPolicyLink}>
-                <a href="/privacypolicy">Privacy Policy</a>
-              </span>
-            </span>
-          )}
-        </CaptionText>
+        
       </Grid>
     </Paper>
   )
