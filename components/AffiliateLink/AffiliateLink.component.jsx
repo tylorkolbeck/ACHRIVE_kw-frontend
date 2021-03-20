8a8adb07149ef35643a9887eac4fa6a54050a86d
jkaplanis <johntkaplanis@gmail.com>
2021-03-20 11:29:39 -0700

    update affiliate links to maintain the same height, added transparent background to accomdate various logo colors

--- components/AffiliateLink/AffiliateLink.component.jsx
+++ components/AffiliateLink/AffiliateLink.component.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 export default function AffiliateLink({ label, url, imageUrl }) {
   return (
-    <a href={url} target="_blank">
-      <img style={{ width: '120px' }} src={imageUrl} />
+    <a href={url} target="_blank" >
+      <img src={imageUrl} alt={label}/>
     </a>
   )
 }
